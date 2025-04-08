cee29d4384c59c2ce866d1ea43aa7f5af18a5042
Anand G Murugan <anand@gamechangesns.com>
2025-04-08 17:56:25 +0530

    feat: adds CORS middleware

--- src/app.py
+++ src/app.py
@@ -3,11 +3,24 @@ from fastapi import FastAPI, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from utils.verifyaccesstoken import VerifyAccessTokenUtil, VerifiedToken, JwtSubjectType
 from routes import health, pdf, video, stream_chat
+from fastapi.middleware.cors import CORSMiddleware
 from asgi_correlation_id import CorrelationIdMiddleware
 
 app = FastAPI(title='gc-pdf-util service', version='0.0.1')
 app.add_middleware(CorrelationIdMiddleware)
 
+origins = [
+    "http://localhost",
+    "https://document-validation.gamechange.dev"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 bearerScheme = HTTPBearer()
 
