ee3a17242f2b0d5b2cc5802b606e5f97152d8839
Anand G Murugan <anand@gamechangesns.com>
2025-05-22 12:45:47 +0530

    feat: adds get class schedule API

--- src/colearn_chat.py
+++ src/colearn_chat.py
@@ -23,6 +23,9 @@ class ChatItem(BaseModel):
 class ChatRequest(BaseModel):
     messages: List[ChatItem]
 
+class GetClassScheduleRequest(BaseModel):
+    message: str
+
 if(configs.LOAD_COLEARN == True):
     # Load documents from the data/docs directory
     documents = [Document(content=item) for item in colearnKnowledgeMdStrings]
@@ -89,4 +92,58 @@ def colearnChat(req: ChatRequest) -> str:
     
     return json.dumps(json_response)
     # return json_response.message
+
+
+import requests, httpx
+
+
+def get_class_schedule(kelas: int, sem: int= 2, year: int= 2025, curriculum: str = "Kurikulum Merdeka", subject: str = "Kimia") -> str:
+    """Use this function to get schedule.
+
+    Args:
+        num_stories (int): Number of stories to return. Defaults to 10.
+        `kelas` (int): Specifies the class or grade level. Example: `kelas=12` for grade 12. 
+        `sem` (int): Indicates the semester. Example: `sem=2` for the second semester.
+        `year` (int): The academic year for which the schedule is being fetched. Example: `year=2025`.
+        `curriculum` (str): Specifies the curriculum type. Example: `curriculum=Kurikulum Merdeka`.
+        `subject` (str): The subject for which the schedule is being requested. Example: `subject=Kimia`.
+
+    Returns:
+        str: JSON string of following format
+        { 
+            schedule: [{
+                "days": "Senin",
+                "time": "17:15 - 18:15",
+                "teacher": "Kak Windi",
+                "seatsLeft": 70,
+                "classStarted": true,
+                "subject": "Kimia Merdeka - Kelas 12 SMT 2",
+                "course_id": "e265144e-b747-4bb1-aab1-377e7ad96828"
+            }]
+        }
+    """
+    url = "https://e2oc2ege54.execute-api.ap-southeast-1.amazonaws.com/slotschedule?kelas={}&sem={}&year={}&curriculum={}&subject={}".format(kelas, sem, year, curriculum, subject)
+
+
+    response = requests.request("GET", url, headers={}, data={})
+
+    return json.dumps({
+        "schedule": response.json()
+    })
+
+
+def get_class_chedule_from_agent(req: GetClassScheduleRequest) -> str:
+    agent = Agent(tools=[get_class_schedule], show_tool_calls=True, markdown=False, 
+            #   instructions=["Do not process response, give it as you receive from get_class_schedule"]
+            )
+    userMessage = req.message
+    response_string = agent.run(userMessage, stream=False)
+    return response_string.content
+
+
+
+
+
+
+
 
--- src/routes/colearn_faq_chat.py
+++ src/routes/colearn_faq_chat.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from colearn_chat import colearnChat, ChatItem, ChatRequest
+from colearn_chat import colearnChat, ChatItem, ChatRequest, get_class_chedule_from_agent, GetClassScheduleRequest
 router = APIRouter(prefix="/colearn-chat")
 
 @router.post("/faq")
@@ -11,4 +11,10 @@ def colearnFaqChat(req: ChatRequest) -> ChatItem:
         content=response_string
     )
 
+    return response
+
+
+@router.post("/get-class-schedule")
+def get_class_schedule(req: GetClassScheduleRequest) -> str:
+    response = get_class_chedule_from_agent(req)
     return response
